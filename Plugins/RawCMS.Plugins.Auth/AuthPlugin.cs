5402eb5faeb3b75ba6b950aaee8cc168f97bc3a2
zmn <zeppaman@altervista.org>
2018-06-28 23:53:14 +0200

    tuning on hardcoded configuration

--- Plugins/RawCMS.Plugins.Auth/AuthPlugin.cs
+++ Plugins/RawCMS.Plugins.Auth/AuthPlugin.cs
@@ -7,11 +7,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using RawCMS.Library.Core;
 using RawCMS.Plugins.Auth.Model;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using IdentityServer4.Models;
+using IdentityServer4.AccessTokenValidation;
+using AspNetCore.Identity.MongoDbCore;
+using AspNetCore.Identity.MongoDbCore.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace RawCMS.Plugins.Auth
 {
@@ -74,30 +82,86 @@ namespace RawCMS.Plugins.Auth
             //)
             //.AddDefaultTokenProviders();
 
+            var mongoDbIdentityConfiguration = new MongoDbIdentityConfiguration
+            {
+                MongoDbSettings = new MongoDbSettings
+                {
+                    ConnectionString = "mongodb://localhost:27017",
+                    DatabaseName = "MongoDbTests"
+                },
+                IdentityOptionsAction = options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 8;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+
+                    // Lockout settings
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                    options.Lockout.MaxFailedAccessAttempts = 10;
+
+                    // ApplicationUser settings
+                    options.User.RequireUniqueEmail = true;
+                    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@.-_";
+                }
+            };
+
+
             services.AddIdentityServer(x =>
             {
                 x.Endpoints.EnableAuthorizeEndpoint = true;
             })
               .AddDeveloperSigningCredential()
               .AddInMemoryApiResources(Config.GetApiResources())
-              .AddInMemoryClients(Config.GetClients()).
-              AddTestUsers(new List<IdentityServer4.Test.TestUser>() {
+              .AddInMemoryPersistedGrants()
+
+
+              .AddInMemoryClients(Config.GetClients())
+           
+              .AddTestUsers(new List<IdentityServer4.Test.TestUser>() {
                   new IdentityServer4.Test.TestUser()
                   {
                       Username="prova",
                       Password="prova",
                       IsActive=true,
                       SubjectId=Guid.NewGuid().ToString(),
-                       ProviderName="HardCoded"
+                       ProviderName="HardCoded",
+                       Claims= new List<Claim>()
+                       {
+                          new Claim(ClaimTypes.Name,"valore")
+                          {
+                              
+                          }
+                       }
+
                   }
               });
+
+            
+            services.AddAuthentication(o=> {
+                {
+                    o.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                    o.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                }
+            }).AddIdentityServerAuthentication(options =>
+            {
+                options.Authority = "http://localhost:50093";
+                options.RequireHttpsMetadata = false;
+
+                options.ApiName = "api1";
+            });
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
         }
 
         public override void Configure(IApplicationBuilder app, AppEngine appEngine)
         {
             base.Configure(app, appEngine);
 
-            app.UseIdentityServer();
+            
+            app.UseAuthentication()
+            .UseIdentityServer();
         }
     }
 }
