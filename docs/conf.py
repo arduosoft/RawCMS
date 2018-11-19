c5951cd1954a1901afadd3a7a213c60a1eada5fd
Daniele Fontani <zeppaman@users.noreply.github.com>
2018-11-19 21:49:01 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,10 @@
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+
 def setup(app):
     app.add_stylesheet('https://media.readthedocs.org/css/sphinx_rtd_theme.css')
