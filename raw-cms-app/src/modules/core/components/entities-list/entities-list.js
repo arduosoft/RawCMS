5cd612f44512a294cdf93341e9f515351898ebba
Leonardo Marcucci <marcuson.nn90@gmail.com>
2019-10-26 13:23:54 +0200

    Add delete button to entities list with confirmation dialog.

--- raw-cms-app/src/modules/core/components/entities-list/entities-list.js
+++ raw-cms-app/src/modules/core/components/entities-list/entities-list.js
@@ -1,5 +1,6 @@
 import { epicSpinners } from '../../../../utils/spinners.js';
 import { entitiesSchemaService } from '../../services/entities-schema.service.js';
+import { snackbarService } from '../../services/snackbar-service.js';
 
 const _EntitiesList = async (res, rej) => {
   const tpl = await RawCMS.loadComponentTpl(
@@ -17,19 +18,52 @@ const _EntitiesList = async (res, rej) => {
       return {
         isLoading: true,
         entities: [],
+        currentEntity: {},
+        isDeleteConfirmVisible: false,
       };
     },
     methods: {
-      fetchEntities: function() {
-        console.log('fetching...');
-        setTimeout(async () => {
-          const res = await entitiesSchemaService.getEntities();
-          this.entities = res;
-          this.isLoading = false;
-        }, 1000);
+      fetchEntities: async function() {
+        const res = await entitiesSchemaService.getEntities();
+        this.entities = res.map(x => {
+          return { ...x, _meta_: { isDeleting: false } };
+        });
+        this.isLoading = false;
       },
       goTo: function(entityId) {
         this.$router.push({ name: 'entity-details', params: { id: entityId } });
+      },
+      showDeleteConfirm: function(entity) {
+        this.currentEntity = entity;
+        this.isDeleteConfirmVisible = true;
+      },
+      dismissDeleteConfirm: function() {
+        this.isDeleteConfirmVisible = false;
+      },
+      deleteEntity: async function(entity) {
+        this.dismissDeleteConfirm();
+
+        entity._meta_.isDeleting = true;
+        const res = await entitiesSchemaService.deleteEntity(entity._id);
+
+        entity._meta_.isDeleting = false;
+        if (!res) {
+          snackbarService.showMessage({
+            color: 'error',
+            message: this.$t('core.entities.deleteErrorMsgTpl', {
+              entityName: entity.CollectionName,
+            }),
+          });
+          return;
+        }
+
+        this.entities = this.entities.filter(x => x._id !== entity._id);
+        snackbarService.showMessage({
+          color: 'success',
+          message: this.$t('core.entities.deletedMsgTpl', {
+            entityName: entity.CollectionName,
+          }),
+        });
       },
     },
     template: tpl,
