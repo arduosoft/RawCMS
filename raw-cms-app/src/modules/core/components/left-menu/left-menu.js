3298fd09adee95445c5408634e54f9bbec2eccd4
Leonardo Marcucci <marcuson.nn90@gmail.com>
2019-11-14 17:43:43 +0100

    Remove top-right menu button and move all to left sidebar.

--- raw-cms-app/src/modules/core/components/left-menu/left-menu.js
+++ raw-cms-app/src/modules/core/components/left-menu/left-menu.js
@@ -1,26 +1,41 @@
 import { RawCMS } from '../../../../config/raw-cms.js';
+import { vuexStore } from '../../../../config/vuex.js';
 import { evtToggleDrawer } from '../../events.js';
+import { loginService } from '../../services/login.service.js';
+import { UserAvatarDef } from '../user-avatar/user-avatar.js';
 
 const _LeftMenu = async (resolve, reject) => {
   const tpl = await RawCMS.loadComponentTpl(
     '/modules/core/components/left-menu/left-menu.tpl.html'
   );
+  const avatarDef = await UserAvatarDef();
 
   resolve({
+    components: {
+      UserAvatar: avatarDef,
+    },
+    computed: {
+      userinfo: function() {
+        return vuexStore.state.core.userInfo;
+      },
+    },
     data: () => {
       return {
         isVisible: false,
+        isUserMenuVisible: false,
         items: [
           { icon: 'mdi-account', text: 'Users', route: 'users' },
           { icon: 'mdi-cube', text: 'Entities', route: 'entities' },
-          { icon: 'mdi-circle', text: 'Lambdas', route: 'lambda' },
-          { icon: 'mdi-square', text: 'TestDialog', route: 'testdialog' },
+          { icon: 'mdi-circle', text: 'Lambdas', route: 'lambda-list' },
         ],
       };
     },
     methods: {
       toggleVisibility: function() {
         this.isVisible = !this.isVisible;
+      },
+      toggleUserMenuVisibility: function() {
+        this.isUserMenuVisible = !this.isUserMenuVisible;
       },
       goTo: function(item) {
         if (this.isActive(item)) {
@@ -30,6 +45,9 @@ const _LeftMenu = async (resolve, reject) => {
       },
       isActive: function(item) {
         return item.route === this.$route.name;
+      },
+      logout: async function() {
+        loginService.logout();
       },
     },
     mounted: function() {
