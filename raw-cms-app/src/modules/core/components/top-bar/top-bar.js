1793d1313b553289e0d3ebdf6a457a1a52b3880a
Leonardo Marcucci <marcuson.nn90@gmail.com>
2019-11-14 09:30:35 +0100

    Fix logout on topbar menu.

--- raw-cms-app/src/modules/core/components/top-bar/top-bar.js
+++ raw-cms-app/src/modules/core/components/top-bar/top-bar.js
@@ -1,5 +1,6 @@
 import { RawCMS } from '../../../../config/raw-cms.js';
 import { evtToggleDrawer } from '../../events.js';
+import { loginService } from '../../services/login.service.js';
 
 const _TopBar = async (resolve, reject) => {
   const tpl = await RawCMS.loadComponentTpl('/modules/core/components/top-bar/top-bar.tpl.html');
