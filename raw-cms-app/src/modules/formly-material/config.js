bc784ae06d280b7d2c02b290228f2a9bf2d035a6
Leonardo Marcucci <marcuson.nn90@gmail.com>
2019-11-25 14:21:53 +0100

    Add config method for formly module.

---
+++ raw-cms-app/src/modules/formly-material/config.js
@@ -0,0 +1,7 @@
+import { TextField } from './components/text-field/text-field.js';
+
+const _configFormlyMaterialModule = function() {
+};
+
+export const configFormlyMaterialModule = _configFormlyMaterialModule;
+export default _configFormlyMaterialModule;
