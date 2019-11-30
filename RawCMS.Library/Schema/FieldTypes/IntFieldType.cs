4bc18b22a7457b823d1710639c87be4636899dc8
Leonardo Marcucci <marcuson.nn90@gmail.com>
2019-11-30 11:20:51 +0100

    Add missing options for int validation.

--- RawCMS.Library/Schema/FieldTypes/IntFieldType.cs
+++ RawCMS.Library/Schema/FieldTypes/IntFieldType.cs
@@ -3,5 +3,20 @@
     public class IntFieldType : FieldType
     {
         public override string TypeName => "int";
+
+        public IntFieldType()
+        {
+            OptionParameter.Add(new OptionParameter()
+            {
+                Name = "max",
+                Type = FieldBaseType.Int
+            });
+
+            OptionParameter.Add(new OptionParameter()
+            {
+                Name = "min",
+                Type = FieldBaseType.Int
+            });
+        }
     }
 }
