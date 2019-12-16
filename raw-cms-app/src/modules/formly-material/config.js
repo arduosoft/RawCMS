14ec6938825f121e970020fd32887f8e764d2c85
Leonardo Marcucci <marcuson.nn90@gmail.com>
2019-12-16 16:10:20 +0100

    Add list field type (FE).

--- raw-cms-app/src/modules/formly-material/config.js
+++ raw-cms-app/src/modules/formly-material/config.js
@@ -1,6 +1,7 @@
 import { BoolField } from './components/bool-field/bool-field.js';
 import { DateField } from './components/date-field/date-field.js';
 import { IntField } from './components/int-field/int-field.js';
+import { ListField } from './components/list-field/list-field.js';
 import { NumberField } from './components/number-field/number-field.js';
 import { TextField } from './components/text-field/text-field.js';
 
@@ -15,6 +16,8 @@ const _configFormlyMaterialModule = function() {
   Vue.$formly.addType('int', IntField);
   // Date/Time
   Vue.$formly.addType('date', DateField);
+  // List
+  Vue.$formly.addType('list', ListField);
 };
 
 export const configFormlyMaterialModule = _configFormlyMaterialModule;
