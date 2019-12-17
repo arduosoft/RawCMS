cdaf8f46b69a680e1f21753008f041c450f0f0c1
Leonardo Marcucci <marcuson.nn90@gmail.com>
2019-12-17 17:40:31 +0100

    Add link to home in left menu.

--- raw-cms-app/src/modules/core/components/left-menu/left-menu.js
+++ raw-cms-app/src/modules/core/components/left-menu/left-menu.js
@@ -24,6 +24,7 @@ const _LeftMenu = async (resolve, reject) => {
         isVisible: false,
         isUserMenuVisible: false,
         items: [
+          { icon: 'mdi-home', text: 'Home', route: 'home' },
           { icon: 'mdi-account', text: 'Users', route: 'users' },
           { icon: 'mdi-cube', text: 'Entities', route: 'entities' },
           { icon: 'mdi-book-open', text: 'Collections', route: 'collections' },
