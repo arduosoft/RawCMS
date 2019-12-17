e95346ce274a695e1ba60088ce5e9851405a3750
Leonardo Marcucci <marcuson.nn90@gmail.com>
2019-12-17 17:42:09 +0100

    Add dashboard to home.

--- raw-cms-app/src/modules/core/views/home-view/home-view.js
+++ raw-cms-app/src/modules/core/views/home-view/home-view.js
@@ -1,7 +1,13 @@
+import { DashboardDef } from '../../components/dashboard/dashboard.js';
+
 const _HomeView = async (res, rej) => {
   const tpl = await RawCMS.loadComponentTpl('/modules/core/views/home-view/home-view.tpl.html');
+  const dashboardDef = await DashboardDef();
 
   res({
+    components: {
+      Dashboard: dashboardDef,
+    },
     template: tpl,
   });
 };
