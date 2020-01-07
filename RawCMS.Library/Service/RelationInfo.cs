0343a4491d3cd3021e1fe996970fb4738f0b10fa
Leonardo Marcucci <marcuson.nn90@gmail.com>
2020-01-07 08:58:26 +0100

    Fix CodeFactor.

---
+++ RawCMS.Library/Service/RelationInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+
+namespace RawCMS.Library.Service
+{
+    public class RelationInfo
+    {
+        public bool IsMultiple { get; set; }
+        public string LookupCollection { get; set; }
+        public List<BsonObjectId> Values { get; set; }
+    }
+}
