0aaabe7e5b9914189dd23c317ef9b9fce92b7b8a
Andrea <andrea17799@yahoo.it>
2020-01-31 12:48:14 +0100

    api-log-collection (WIP)

---
+++ Plugins/RawCMS.Plugins.FullText/Controllers/LogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RawCMS.Library.Core.Attributes;
+using RawCMS.Plugins.Core.Model;
+using RawCMS.Plugins.FullText.Core;
+using System;
+using System.Collections.Generic;
+using RawCMS.Plugins.FullText.Models;
+namespace RawCMS.Plugins.FullText.Controllers
+{
+    [Route("api")]
+    class LogController : Controller
+    {
+       
+        [HttpGet()]
+        [Route("write")]
+        public void write()
+        {
+            Console.WriteLine("ciao");
+            }
+    }
+        
+}
+
---
+++ Plugins/RawCMS.Plugins.FullText/Models/LogEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace RawCMS.Plugins.FullText.Models
+{
+    public class LogEntity 
+    {
+        public DateTime CreateDate { get; set; }
+        public string Message { get; set; }
+        public string Level { get; set; }
+        public Guid Id { get; set; }
+        public long ApplicationId { get; set; }
+
+    }
+}
