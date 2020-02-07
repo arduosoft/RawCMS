60c2c6dafd0bbd4d65ea274a0f875f6b7e6f86bc
Daniele Fontani <zeppaman@altervista.org>
2020-02-07 16:26:05 +0100

    support ga

--- docker/config/ui/bootstrap.sh
+++ docker/config/ui/bootstrap.sh
@@ -17,3 +17,13 @@ then
 else 
  echo "\$PORT is empty, used default port 80"
 fi
+
+ls  /usr/share/nginx/html
+
+if [ ! -z "$GOOGLE_ANALITYCS" ]
+then
+ echo "settings google analytic for $GOOGLE_ANALITYCS (no regex)"
+ GA="<script async src=\"https://www.googletagmanager.com/gtag/js?id=$GOOGLE_ANALITYCS\"></script> <script> window.dataLayer = window.dataLayer \|\| []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '$GOOGLE_ANALITYCS'); </script>"
+ sed -i -e "s|</body>|$GA|g" /usr/share/nginx/html/index.html
+
+fi
